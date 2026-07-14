fdd5bd1291c043f0ebb35575b3c93a761a01d50a
Grandjean <mathis.grandjean@edhec.com>
2026-07-14 08:54:08 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,7 @@ Created on Sun Jul 12 18:15:37 2026
 
 @author: Mathis
 """
-
-
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
-restaurant_DIR = BASE_DIR / "restaurant_Chipotle"
-job_DIR = BASE_DIR / "job_offer_Chipotle"
+
